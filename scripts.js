5797c3516df40f02f594347085947361eed53b28
Yaswanth <pyaswanth4@gmail.com>
2023-03-11 19:29:33 +0530

    Rock Paper Scissor game with a UI can be more efficient for now its functional

--- scripts.js
+++ scripts.js
@@ -1,4 +1,45 @@
 // Rock Paper Scissor Played from the console
+const humanScore = document.querySelector(".humanScore");
+const computerScore = document.querySelector(".computerScore");
+const draw = document.querySelector(".draw");
+const btns = document.querySelectorAll("button");
+let player = 0;
+let computer = 0;
+btns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    let playerSelection = this.value;
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    if (result == "computer") {
+      computer++;
+      computerScore.textContent = `Computer Score : ${computer}`;
+      console.log("COMPUTER has won this Round");
+      draw.textContent = ``;
+    } else if (result == "player") {
+      player++;
+      humanScore.textContent = `Human Score : ${player}`;
+      console.log("PLAYER has won this Round");
+      draw.textContent = ``;
+    } else if (result == "draw") {
+      draw.textContent = `It's a DRAW play again`;
+      console.log("It's a DRAW play again");
+    }
+
+    if (player == 5) {
+      draw.textContent = `Player has WON the game`;
+      computer = 0;
+      player = 0;
+      computerScore.textContent = `Computer Score : ${computer}`;
+      humanScore.textContent = `Human Score : ${player}`;
+    } else if (computer == 5) {
+      draw.textContent = `Computer has WON the game`;
+      computer = 0;
+      player = 0;
+      computerScore.textContent = `Computer Score : ${computer}`;
+      humanScore.textContent = `Human Score : ${player}`;
+    }
+  })
+);
 
 //function to generate a computer choice
 
@@ -33,36 +74,40 @@ function playRound(playerSelection, computerSelection) {
     return "player";
   } else if ((playerSelection == "rock") & (computerSelection == "paper")) {
     return "computer";
+  } else if (playerSelection == computerSelection) {
+    return "draw";
   }
 }
 //
 //
 //
 //function that plays 5 rounds and declare the winner
-function game() {
-  let player = 0;
-  let computer = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Enter your sign (rock,paper,scissor)");
-    let computerSelection = getComputerChoice();
-    console.log(
-      `computers choice is ${computerSelection}, players choice is ${playerSelection}`
-    );
-    let result = playRound(playerSelection, computerSelection);
+// function game() {
+//   let player = 0;
+//   let computer = 0;
+//   for (let i = 0; i < 5; i++) {
+//     //let playerSelection = prompt("Enter your sign (rock,paper,scissor)");
+//     let computerSelection = getComputerChoice();
+//     console.log(
+//       `computers choice is ${computerSelection}, players choice is ${playerSelection}`
+//     );
+//     let result = playRound(playerSelection, computerSelection);
 
-    if (result == "player") {
-      player++;
-    } else if (result == "computer") {
-      computer++;
-    }
-  }
-  if (player > computer) {
-    console.log("Player Is the winner");
-  } else if (player == computer) {
-    console.log("It's A draw play again");
-  } else {
-    console.log("Computer is the winner");
-  }
-}
+//     if (result == "player") {
+//       player++;
+//       console.log(player);
+//     } else if (result == "computer") {
+//       computer++;
+//       console.log(computer);
+//     }
+//   }
+//   if (player > computer) {
+//     console.log("Player Is the winner");
+//   } else if (player == computer) {
+//     console.log("It's A draw play again");
+//   } else {
+//     console.log("Computer is the winner");
+//   }
+// }
 
-game();
+//game();
